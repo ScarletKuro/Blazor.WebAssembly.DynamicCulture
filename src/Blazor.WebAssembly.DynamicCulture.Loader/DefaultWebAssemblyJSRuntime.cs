49ea24e007eabdf0ff9f5d908f7f4fb7b3f84ba6
ScarletKuro <artem.melnikov@live.com>
2022-05-30 19:57:47 +0300

    Init

---
+++ src/Blazor.WebAssembly.DynamicCulture.Loader/DefaultWebAssemblyJSRuntime.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop.WebAssembly;
+
+namespace Blazor.WebAssembly.DynamicCulture.Loader;
+
+public sealed class DefaultWebAssemblyJSRuntime : WebAssemblyJSRuntime
+{
+    internal static readonly DefaultWebAssemblyJSRuntime Instance = new();
+
+    public ElementReferenceContext ElementReferenceContext { get; }
+
+    DefaultWebAssemblyJSRuntime()
+    {
+        ElementReferenceContext = new WebElementReferenceContext(this);
+        JsonSerializerOptions.Converters.Add(new ElementReferenceJsonConverter(ElementReferenceContext));
+    }
+}
